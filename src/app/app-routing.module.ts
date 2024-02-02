455c6764b89f59b19db6e153284ac57e799cb17b
Ink230 <Ink230@users.noreply.github.com>
2024-02-02 04:03:35 -0500

    initial irongoon base

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IrongoonComponent } from './components/irongoon/irongoon.component';
 
-const routes: Routes = [{ path: 'test', component: IrongoonComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: IrongoonComponent,
+  },
+  {
+    path: 'irongoon',
+    redirectTo: '',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/irongoon/irongoon.component.ts
+++ src/app/components/irongoon/irongoon.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-irongoon',
   templateUrl: './irongoon.component.html',
-  styleUrls: ['./irongoon.component.css']
+  styleUrls: ['./irongoon.component.css'],
 })
 export class IrongoonComponent {
-
+  tabs: any[] = [
+    { id: 1, title: 'test' },
+    { id: 2, title: 'test' },
+    { id: 3, title: 'test' },
+  ];
 }
