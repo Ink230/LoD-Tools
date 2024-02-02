d8701bcaa2a9bbc93e6af5c4ec84923838efbbaf
Ink230 <Ink230@users.noreply.github.com>
2024-02-02 03:35:44 -0500

    configure routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IrongoonComponent } from './components/irongoon/irongoon.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'test', component: IrongoonComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/components/irongoon/irongoon.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { IrongoonComponent } from './irongoon.component';
+
+describe('IrongoonComponent', () => {
+  let component: IrongoonComponent;
+  let fixture: ComponentFixture<IrongoonComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [IrongoonComponent]
+    });
+    fixture = TestBed.createComponent(IrongoonComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
