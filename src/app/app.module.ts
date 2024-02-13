1ac79cd6d6400cdb2f3e052e20553aabd928d82a
Ink230 <Ink230@users.noreply.github.com>
2024-02-13 12:17:26 -0500

    initial work on custom dropdown component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { ClipboardModule } from 'ngx-clipboard';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IrongoonDropdownComponent } from './components/irongoon-dropdown/irongoon-dropdown.component';
 import { DisplayCategoryIrongoonComponent } from './components/irongoon/display-category-irongoon/display-category-irongoon.component';
 import { IrongoonComponent } from './components/irongoon/irongoon.component';
 import { IrongoonService } from './services/irongoon.service';
 
 @NgModule({
-  declarations: [AppComponent, IrongoonComponent, DisplayCategoryIrongoonComponent],
+  declarations: [AppComponent, IrongoonComponent, DisplayCategoryIrongoonComponent, IrongoonDropdownComponent],
   imports: [BrowserModule, AppRoutingModule, ClipboardModule, FormsModule],
   providers: [IrongoonService],
   bootstrap: [AppComponent],
--- src/app/components/irongoon-dropdown/irongoon-dropdown.component.ts
+++ src/app/components/irongoon-dropdown/irongoon-dropdown.component.ts
@@ -2,11 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-irongoon-dropdown',
-  standalone: true,
-  imports: [],
   templateUrl: './irongoon-dropdown.component.html',
-  styleUrl: './irongoon-dropdown.component.css'
+  styleUrl: './irongoon-dropdown.component.css',
 })
 export class IrongoonDropdownComponent {
+  list = [
+    { id: 1, name: 'first' },
+    { id: 2, name: 'second' },
+    { id: 3, name: 'third' },
+  ];
 
+  selected = {id: 1, name: 'first' }
 }
