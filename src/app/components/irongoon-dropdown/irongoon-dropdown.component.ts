b03460dd9288ef2ff4e9a5b55b2f8377cd4fbfe9
Ink230 <Ink230@users.noreply.github.com>
2024-02-14 15:15:43 -0500

    implement clickthrough anti-container for custom dropdown menus

--- src/app/components/irongoon-dropdown/irongoon-dropdown.component.ts
+++ src/app/components/irongoon-dropdown/irongoon-dropdown.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-irongoon-dropdown',
@@ -12,5 +12,40 @@ export class IrongoonDropdownComponent {
     { id: 3, name: 'third' },
   ];
 
-  selected = {id: 1, name: 'first' }
+  selected = this.list[0];
+
+  displayDropdown = false;
+
+  id = this.generateUUID();
+  dropdownIdentifier = `dropdown-identifier-${this.id}`
+
+  @HostListener('document:click', ['$event'])
+  onClick(event: MouseEvent) {
+    if ((event.target as HTMLElement).id === this.dropdownIdentifier) {
+      const nested = document.elementFromPoint(event.clientX, event.clientY) as HTMLElement;
+      if (nested) {
+        nested.click();
+      }
+    }
+  }
+
+  toggleDropdown() {
+    this.displayDropdown = !this.displayDropdown;
+  }
+
+  closeDropdown() {
+    this.displayDropdown = false;
+  }
+
+  updateSelectedOption(item: any) {
+    this.selected = item;
+  }
+
+  generateUUID(): string {
+      return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        const r = (Math.random() * 16) | 0,
+          v = c == 'x' ? r : (r & 0x3) | 0x8;
+        return v.toString(16);
+      });
+  }
 }
