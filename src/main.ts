0511b468db0b04bf96f4c13cce0987f6855bd0ff
Ink230 <Ink230@users.noreply.github.com>
2024-03-16 12:48:36 -0400

    step 3 of standalone conversion

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,19 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+
+import { importProvidersFrom } from '@angular/core';
+import { AppComponent } from './app/app.component';
+import { FormsModule } from '@angular/forms';
+import { ClipboardModule } from 'ngx-clipboard';
+import { AppRoutingModule } from './app/app-routing.module';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { IrongoonService } from './app/services/irongoon.service';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(BrowserModule, AppRoutingModule, ClipboardModule, FormsModule),
+        IrongoonService
+    ]
+})
   .catch(err => console.error(err));
