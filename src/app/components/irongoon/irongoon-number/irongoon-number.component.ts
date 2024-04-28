5b684ad5dc78fbc1279d6654b22e39b05fda2e78
Ink230 <Ink230@users.noreply.github.com>
2024-04-28 05:32:31 -0400

    add binding to numeric text input

--- src/app/components/irongoon/irongoon-number/irongoon-number.component.ts
+++ src/app/components/irongoon/irongoon-number/irongoon-number.component.ts
@@ -1,11 +1,12 @@
 import { Component, input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { NumericInputDirective } from 'src/app/directives/numeric-input.directive';
 import { IrongoonOption } from 'src/app/models/irongoon.model';
 
 @Component({
   selector: 'app-irongoon-number',
   standalone: true,
-  imports: [NumericInputDirective],
+  imports: [FormsModule, NumericInputDirective],
   templateUrl: './irongoon-number.component.html',
   styleUrl: './irongoon-number.component.css',
 })
