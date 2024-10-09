e16f1d8b8370031bf1799155eb075f5362fbf712
Ink230 <Ink230@users.noreply.github.com>
2024-10-09 04:02:16 -0400

    add character-comparison attribute styling and initial mock data

--- src/app/components/game-data/character-comparison/character-comparison.component.ts
+++ src/app/components/game-data/character-comparison/character-comparison.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { BehaviorSubject } from 'rxjs';
+import { Body } from 'src/app/models/game-data.model';
 import { GameDataService } from 'src/app/services/game-data.service';
+import { GraphDisplayComponent } from '../../graph-display/graph-display.component';
 
 @Component({
   selector: 'app-character-comparison',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, GraphDisplayComponent],
   templateUrl: './character-comparison.component.html',
   styleUrl: './character-comparison.component.css',
 })
@@ -23,6 +26,9 @@ export class CharacterComparisonComponent implements OnInit {
     HP: 'HP',
   };
 
+  rowData = new BehaviorSubject<Body[]>([]);
+  seriesData = new BehaviorSubject<any>([]);
+
   ngOnInit(): void {
     const characterFormControls = this.getCharacterNames().reduce((controls, character) => {
       controls[character.id] = this.formBuilder.group({
@@ -37,7 +43,18 @@ export class CharacterComparisonComponent implements OnInit {
     }, {});
     this.characterSelections = this.formBuilder.group(characterFormControls);
 
-    this.characterSelections.valueChanges.pipe().subscribe((c) => console.log(c));
+    this.rowData.next([
+      { level: 0, attack: 2, defense: 4, speed: 40, magicAttack: 4, magicDefense: 2, magicAvoid: 0, magicHit: 0, attackHit: 0, attackAvoid: 0, hp: 500 },
+      { level: 1, attack: 5, defense: 4, speed: 40, magicAttack: 4, magicDefense: 2, magicAvoid: 0, magicHit: 0, attackHit: 0, attackAvoid: 0, hp: 500 },
+    ]);
+    this.seriesData.next([{ type: 'line', xKey: 'level', yKey: 'attack' }]);
+
+    this.characterSelections.valueChanges.pipe().subscribe((c) => {
+      // loop through controls
+      // pull char data for any control on
+      // build rowData
+      // build series
+    });
   }
 
   getCharacterNames(): { firstName: string; id: number }[] {
